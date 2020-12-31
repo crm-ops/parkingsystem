d6ed45bec2e44064e5aebe01475ed6d54ee2a903
crm-ops <sv@crm-ops.com>
2020-12-31 18:54:15 +0100

    P5 - svige - 28/12/2020
    - fixed unit test by implmeenting jodatime
    - fixed existing integration test by adding junit maven dependency
    - added a funcional testing plan to be implemented

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -2,6 +2,9 @@ package com.parkit.parkingsystem.service;
 
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
+import org.joda.time.DateTime;
+//implementing jodatime for simplified duration calculations
+import org.joda.time.Duration;
 
 public class FareCalculatorService {
 
@@ -10,19 +13,31 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        DateTime indatetime = new DateTime(ticket.getInTime());
+        DateTime outdatetime = new DateTime(ticket.getOutTime());
+
+        //int inHour = ticket.getInTime().getHours();
+        //int outHour = ticket.getOutTime().getHours();
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+
+        Duration parkingduration = new Duration(indatetime,outdatetime);
+        //int duration = outHour - inHour;
+        // fixing the 45 mins stay unit test - the number of full hours is rounded to 0 due to the int format of duration
+        // switching to a duration calculated in minutes
+        // using jodatime standard duration feature
+        int duration = (int)parkingduration.getStandardMinutes();
+
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                //dividing the houly rate by 60 to get rate by minutes
+                ticket.setPrice(duration * (Fare.CAR_RATE_PER_HOUR/60));
                 break;
             }
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                //dividing the houly rate by 60 to get rate by minutes
+                ticket.setPrice(duration * (Fare.BIKE_RATE_PER_HOUR/60));
                 break;
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -4,6 +4,7 @@ import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -13,6 +14,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Date;
+
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -22,6 +27,7 @@ public class ParkingDataBaseIT {
     private static ParkingSpotDAO parkingSpotDAO;
     private static TicketDAO ticketDAO;
     private static DataBasePrepareService dataBasePrepareService;
+    private static Ticket dummyTicket;
 
     @Mock
     private static InputReaderUtil inputReaderUtil;
@@ -33,17 +39,20 @@ public class ParkingDataBaseIT {
         ticketDAO = new TicketDAO();
         ticketDAO.dataBaseConfig = dataBaseTestConfig;
         dataBasePrepareService = new DataBasePrepareService();
+        dataBasePrepareService.clearDataBaseEntries();
     }
 
     @BeforeEach
     private void setUpPerTest() throws Exception {
         when(inputReaderUtil.readSelection()).thenReturn(1);
         when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
-        dataBasePrepareService.clearDataBaseEntries();
+
     }
 
     @AfterAll
     private static void tearDown(){
+
+
 
     }
 
@@ -52,14 +61,44 @@ public class ParkingDataBaseIT {
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
         //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+        // sebastien VIGE ticket and parking are saved in the Test DB
+        Ticket ticket = ticketDAO.getTicket("ABCDEF");
+        //no assertion
+
     }
 
     @Test
-    public void testParkingLotExit(){
+    public void testParkingLotExit() throws InterruptedException {
         testParkingACar();
+        //added wait time 5s to avoid inTime>outTime during test
+        Thread.sleep(1000);
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processExitingVehicle();
+
         //TODO: check that the fare generated and out time are populated correctly in the database
+        // no assertion
     }
+
+    // test 2 cars entry and one out
+
+    // test the same car/plate entry with an existing open tiket --> should not allow car entry
+
+    // test unknown plate out --> generate a message
+
+    // test 2 bikes entry and one out
+
+    // test 1 car & 1 bike in then out
+
+    // test 30 mins free parking for new client
+
+    // test 5% discount for returning client
+
+    //==> integration test
+
+    //1 - test reports
+    // testing parking service only - not to worry if coverage is not 100%
+    // joda time to impletment in th full class + test + ticket + factorise (at the end)
+    //code coverage in intelliJ
+
 
 }
